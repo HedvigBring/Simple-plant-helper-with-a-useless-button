ec5e12620ecf3913bb095be8398366d1d102d8be
HedvigBring <54798403+HedvigBring@users.noreply.github.com>
2021-08-02 13:43:34 +0200

    Adding files

---
+++ main.py
@@ -0,0 +1,49 @@
+import pycom
+import time
+from machine import Pin, ADC
+from lib.dht import DHT
+
+pycom.heartbeat(False)
+
+pycom.heartbeat(False)
+pycom.rgbled(0xFF0000)  # Red
+th = DHT(Pin('P23', mode=Pin.OPEN_DRAIN), 0)
+btn = Pin('P10', mode=Pin.IN, pull=Pin.PULL_UP)
+
+
+SoilSensorPin = 'P17'
+soilPin = Pin(SoilSensorPin, mode=Pin.IN)
+time.sleep(2)
+
+adc = ADC(bits=10)
+adc.vref(1260)
+apin = adc.channel(attn=ADC.ATTN_11DB, pin=SoilSensorPin)
+
+time.sleep(2)
+
+pycom.rgbled(0x00FF00)  # Green
+
+def btn_callback_pressed(p):
+    pycom.rgbled(0xFF0000)  # Red
+    val = apin()
+    print(val)
+    time.sleep(1)
+
+    pycom.rgbled(0x00FF00)  # Green
+
+btn.callback(Pin.IRQ_RISING, handler=btn_callback_pressed)
+
+while True:
+    result = th.read()
+    val = apin()
+
+    pybytes.send_signal(1, result.temperature)
+    pybytes.send_signal(2, result.humidity)
+    pybytes.send_signal(3, 1023 - val)
+    print('sent signal {}'.format(result.temperature))
+    print('sent signal {}'.format(result.humidity))
+    print("Temperature: %d C" % result.temperature)
+    print("Humidity: %d %%" % result.humidity)
+    print("Value", val)
+
+    time.sleep(600)
